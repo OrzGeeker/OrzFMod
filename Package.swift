6f64eba47e36de7864dc6c0191831045a1f2479a
wangzhizhou <824219521@qq.com>
2021-07-02 01:46:18 +0800

    server on personal server

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "OrzFMod",
-            url: "https://github.com/OrzGeeker/OrzFMod/blob/master/OrzFMod.xcframework.zip",
+            url: "https://download.jokerhub.cn/OrzFMod.xcframework.zip",
             checksum: "2e462b38861183ad60ece41ed6eab3e732e133713cec32e79675c8c17e1ffc9e"),
     ]
 )

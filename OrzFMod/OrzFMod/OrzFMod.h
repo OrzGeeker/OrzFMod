9892edf01539256b1754e571f435094fc8918ae0
wangzhizhou <wangzhizhou@bytedance.com>
2023-04-11 18:37:22 +0800

    change header import type

--- OrzFMod/OrzFMod/OrzFMod.h
+++ OrzFMod/OrzFMod/OrzFMod.h
@@ -6,13 +6,10 @@
 //  Copyright © 2018 joker. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "FModCapsule.h"
 
 //! Project version number for OrzFMod.
 FOUNDATION_EXPORT double OrzFModVersionNumber;
 
 //! Project version string for OrzFMod.
 FOUNDATION_EXPORT const unsigned char OrzFModVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <OrzFMod/PublicHeader.h>
-#import <OrzFMod/FModCapsule.h> 
